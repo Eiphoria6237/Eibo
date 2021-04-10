994b299e5cb7fd03a4969cb78053f68e10b7b002
Eiphoria <liuerbsb@gmal.com>
2021-04-10 16:14:58 +0900

    Initial Commit

---
+++ Eibo/EiboApp.swift
@@ -0,0 +1,17 @@
+//
+//  EiboApp.swift
+//  Eibo
+//
+//  Created by LIU SHURUI on 2021/04/10.
+//
+
+import SwiftUI
+
+@main
+struct EiboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
